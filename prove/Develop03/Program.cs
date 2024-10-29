9fb13c9c2c82d9ef5bc425b8a7215af83320e0cb
mucacran <el.mucacran.rasta@hotmail.com>
2024-10-29 06:19:45 -0500

    cambio de nombre de varianle

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -66,12 +66,12 @@ class Program
 
 
         // Crear la referencia y escritura
-        Reference reference = new Reference("John", 3, 16);
+        Reference reference = new Reference(randomBookName, 3, 16);
         Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
 
 
 
-/*
+
         while (true)
         {
             Console.Clear();
@@ -95,6 +95,6 @@ class Program
                 break;
             }
         }
-        */
+        
     }
 }
