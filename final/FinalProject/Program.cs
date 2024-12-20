3fffb8575120f0200a388e4f31a0d5db17101be2
mucacran <el.mucacran.rasta@hotmail.com>
2024-12-19 22:36:44 -0500

    trabajando

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,60 @@
 using System;
+//are un sistema para reservar en el gimnacio
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        
+        bool running = true;
+        
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflection Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Start Gratitude Activity");
+            Console.WriteLine("  5. Quit");
+            Console.Write("Select a choise from the menu: ");
+            
+            string choice = Console.ReadLine();
+            
+            //Activity activity = null;
+            
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"tu elegistes: {choice}");
+                    //activity = new BreathingActivity();
+                    break;
+                case "2":
+                    Console.WriteLine($"tu elegistes: {choice}");
+                    //activity = new ReflectionActivity();
+                    break;
+                case "3":
+                    Console.WriteLine($"tu elegistes: {choice}");
+                    //activity = new ListingActivity();
+                    break;
+                case "4":
+                    Console.WriteLine($"tu elegistes: {choice}");
+                    //activity = new GratitudeActivity();
+                    break;
+                case "5":
+                    running = false;
+                    continue;
+                default:
+                    Console.WriteLine("Invalid choice. Press enter to continue...");
+                    Console.ReadLine();
+                    continue;
+            }
+            /*
+            if (activity != null)
+            {
+                activity.RunActivity();
+            }
+            */
+        }
     }
 }
